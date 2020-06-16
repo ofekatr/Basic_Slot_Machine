64a15d9676353f08e624c513ce8943e4f9d4bd95
Ofek Atar <ofekatar98@gmail.com>
2020-06-17 02:38:45 +0300

    Added loading bar

--- src/js/scenes/PreloaderScene.js
+++ src/js/scenes/PreloaderScene.js
@@ -22,11 +22,78 @@ import spin_sound_url from '~/assets/Spin.wav';
 const font_name = 'Press Start 2P';
 
 export default class PreloaderScene extends Phaser.Scene {
-    preload() {
+
+    preload = () => {
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(width / 2 - 160, height / 2 - 150, 320, 50);
+        
+        var loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+        
+        var percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+        
+        var assetText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 50,
+            text: '',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        assetText.setOrigin(0.5, 0.5);
+        
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(width / 2 - 160, height / 2 - 145, 300 * value, 40);
+        });
+        
+        this.load.on('fileprogress', function (file) {
+            assetText.setText('Loading asset: ' + file.key);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
+        
+        // this.load.image('logo', logo_url);
+
         this.load_images();
         this.load_audio();
         const fonts = new WebFontFile(this.load, font_name);
         this.load.addFile(fonts);
+
+        // for (var i = 0; i < 500; i++) {
+        //     this.load.image('logo'+i, logo_url);
+        // }
     }
 
     load_audio = () => {
@@ -43,6 +110,7 @@ export default class PreloaderScene extends Phaser.Scene {
     }
 
     create() {
+        var logo = this.add.image(400, 300, 'logo');
         this.scene.start(StateKeys.TITLE_SCENE);
     }
 }
